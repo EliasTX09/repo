c249226eeb39978672913f1053c13edb7274b923
EliasTX09 <eliaswragebr@gmail.com>
2025-06-28 17:50:29 +0200

    neu

--- repo/plugin.video.bundesliga/default.py
+++ repo/plugin.video.bundesliga/default.py
@@ -88,7 +88,7 @@ def list_main_menu():
     xbmcplugin.addDirectoryItem(handle=HANDLE, url=url, listitem=li, isFolder=True)
 
     stream_url = urllib.parse.quote_plus("http://expisyyr.iptv-allindig.com:80/live/EMJHD73R/6YC99F9X/164402.m3u8")
-    plugin_url = f"plugin://plugin.video.bundesliga/?action=play_stream&stream={stream_url}"
+    plugin_url = f"plugin://plugin.video.bundesliga/?action=play&stream={stream_url}"
 
     # 🔴 Name hier anpassen:
     li = xbmcgui.ListItem(label="[B][COLOR red]🔴 Live: Sat 1 HD[/COLOR][/B]")
